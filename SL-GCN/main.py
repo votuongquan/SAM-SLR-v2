8add938419f2817a9b603dc35893b9eb0f5292db
votuongquan2004@gmail.com <votuongquan2004@gmail.com>
2024-04-21 15:49:01 +0700

    fix bug in main.py

--- SL-GCN/main.py
+++ SL-GCN/main.py
@@ -573,7 +573,7 @@ if __name__ == '__main__':
     p = parser.parse_args()
     if p.config is not None:
         with open(p.config, 'r') as f:
-            default_arg = yaml.load(f)
+            default_arg = yaml.load(f, Loader= yaml.FullLoader)
         key = vars(p).keys()
         for k in default_arg.keys():
             if k not in key:
