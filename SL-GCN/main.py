2a92a0c7b62a0a991eb8aff26448d4df6e227729
votuongquan2004@gmail.com <votuongquan2004@gmail.com>
2024-04-22 16:55:46 +0700

    update main.py

--- SL-GCN/main.py
+++ SL-GCN/main.py
@@ -252,19 +252,22 @@ class Processor():
             self.print_log('Load weights from {}.'.format(self.arg.weights))
             if '.pkl' in self.arg.weights:
                 with open(self.arg.weights, 'r') as f:
-                    weights = pickle.load(f)
+                    checkpoint = pickle.load(f)
             else:
-                weights = torch.load(self.arg.weights)
+                checkpint = torch.load(self.arg.weights)
 
              # check weights is for gpu or cpu and change into gpu
-            if 'state_dict' in weights.keys():
-                weights = weights['state_dict']
-            if 'module' in list(weights.keys())[0]:
-                weights = OrderedDict([[k.split('module.')[-1],
-                                        v.cuda(output_device) if torch.is_tensor(v) else v] for k, v in weights.items()])
-            else:
-                weights = OrderedDict([[k, v.cuda(output_device) if torch.is_tensor(v) else v]
-                                       for k, v in weights.items()])
+                model_weights = checkpoint.get('weights', checkpoint)
+
+                # check weights is for gpu or cpu and change into gpu
+                if 'state_dict' in model_weights.keys():
+                    model_weights = model_weights['state_dict']
+                if 'module' in list(model_weights.keys())[0]:
+                    model_weights = OrderedDict([[k.split('module.')[-1],
+                                            v.cuda(output_device) if torch.is_tensor(v) else v] for k, v in model_weights.items()])
+                else:
+                    model_weights = OrderedDict([[k, v.cuda(output_device) if torch.is_tensor(v) else v]
+                                        for k, v in model_weights.items()])
 
             for w in self.arg.ignore_weights:
                 if weights.pop(w, None) is not None:
