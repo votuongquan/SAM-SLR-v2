83fa4b1c80c33883d46127cd530e5b46856e4a32
votuongquan2004@gmail.com <votuongquan2004@gmail.com>
2024-05-13 17:45:33 +0700

    update onnx.py

--- SL-GCN/export_to_onnx.py
+++ SL-GCN/export_to_onnx.py
@@ -6,7 +6,15 @@ from main import import_class
 model_name="VSL_SAM_SLR_V2_joint"
 Model=import_class("model.decouple_gcn_attn.Model")
 model=Model(num_class=100, num_point=27, num_person=1, graph="graph.sign_27.Graph", groups=16,block_size=41,graph_args={'labeling_mode': 'spatial'},in_channels=3).to('cuda')
-model.load_state_dict(torch.load("save_models/vsl_joint_best_model.pt"))
+
+# Load the checkpoint
+checkpoint = torch.load('save_models/vsl_joint_best_model.pt')
+
+weights = checkpoint['weights'] if 'weights' in checkpoint else checkpoint
+# Load the weights into your model
+model.load_state_dict(weights, strict=False)
+
+
 dummy_input = torch.randn(1,3,120,27,1).to('cuda')
 output_path = f'{model_name}.onnx'
 torch.onnx.export(
